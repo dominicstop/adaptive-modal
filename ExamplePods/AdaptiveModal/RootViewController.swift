1b6dcd4da387d8ff629222263c81337e8f47bc20
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-08-25 18:03:44 +0800

    💫 Update: Ex - ExamplePods

---
+++ ExamplePods/AdaptiveModal/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  AdaptiveModal
+//
+//  Created by Dominic Go on 08/25/2023.
+//  Copyright (c) 2023 Dominic Go. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad();
+  };
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning();
+  };
+};
+
