164e72270091225158c87b94a4b9739987dc7390
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-08-25 18:11:19 +0800

    ⚙️ Chore: Ex - Run `pod install`

--- ExamplePods/Pods/Target Support Files/Pods-AdaptiveModal_Example/Pods-AdaptiveModal_Example-frameworks.sh
+++ ExamplePods/Pods/Target Support Files/Pods-AdaptiveModal_Example/Pods-AdaptiveModal_Example-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AdaptiveModal/AdaptiveModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ComputableLayout/ComputableLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AdaptiveModal/AdaptiveModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ComputableLayout/ComputableLayout.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
