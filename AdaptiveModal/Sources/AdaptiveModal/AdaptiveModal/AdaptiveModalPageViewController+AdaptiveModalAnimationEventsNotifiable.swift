81e2db56b27306b8515d9b4afc46b1984dd3a8da
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-09-07 19:26:24 +0800

    🛠 Refactor: Extract Extensions to Own File

---
+++ AdaptiveModal/Sources/AdaptiveModal/AdaptiveModal/AdaptiveModalPageViewController+AdaptiveModalAnimationEventsNotifiable.swift
@@ -0,0 +1,92 @@
+//
+//  AdaptiveModalPageViewController+AdaptiveModalAnimationEventsNotifiable.swift
+//  
+//
+//  Created by Dominic Go on 9/7/23.
+//
+
+import UIKit
+
+
+extension AdaptiveModalPageViewController: AdaptiveModalAnimationEventsNotifiable {
+
+  public func notifyOnModalAnimatorStart(
+    sender: AdaptiveModalManager,
+    animator: UIViewPropertyAnimator?,
+    interpolationPoint: AdaptiveModalInterpolationPoint,
+    isAnimated: Bool
+  ) {
+  
+    let nextSnapPointIndex = interpolationPoint.snapPointIndex;
+    
+    guard let resolvedPages = self.resolvedPages,
+          let nextPage = resolvedPages[safeIndex: nextSnapPointIndex]
+    else { return };
+    
+    let animationBlock = {
+      resolvedPages.enumerated().forEach {
+        guard let pageVC = $0.element.viewController else { return };
+        let shouldShowPage = pageVC ===  nextPage.viewController;
+        pageVC.view.alpha = shouldShowPage ? 1 : 0;
+      };
+    };
+    
+    if isAnimated,
+       let animator = animator {
+      
+      animator.addAnimations(animationBlock);
+      
+    } else {
+      animationBlock();
+    };
+  };
+  
+  public func notifyOnModalAnimatorPercentChanged(
+    sender: AdaptiveModalManager,
+    percent: CGFloat
+  ) {
+  
+    guard let resolvedPages = self.resolvedPages,
+          let interpolationSteps = sender.interpolationSteps
+    else { return };
+    
+    let rangeInput = interpolationSteps.map { $0.percent };
+    
+    resolvedPages.enumerated().forEach {
+      guard let pageVC = $0.element.viewController else { return };
+
+      let outputRangeOpacity = resolvedPages.map {
+        let shouldShowPage = pageVC === $0.viewController;
+        return CGFloat(shouldShowPage ? 1 : 0);
+      };
+      
+      let opacityNext = AdaptiveModalUtilities.interpolate(
+        inputValue: percent,
+        rangeInput: rangeInput,
+        rangeOutput: outputRangeOpacity
+      );
+      
+      let prevOpacity = pageVC.view.alpha;
+      
+      guard let opacityNext = opacityNext,
+            prevOpacity != opacityNext
+      else { return };
+      
+      pageVC.view.alpha = opacityNext;
+    };
+  };
+  
+  public func notifyOnModalAnimatorStop(
+    sender: AdaptiveModalManager
+  ) {
+    // no-op
+  };
+  
+  public func notifyOnModalAnimatorCompletion(
+    sender: AdaptiveModalManager,
+    position: UIViewAnimatingPosition
+  ) {
+    // no-op
+  };
+};
+
---
+++ AdaptiveModal/Sources/AdaptiveModal/AdaptiveModal/AdaptiveModalPageViewController+AdaptiveModalPresentationEventsNotifiable.swift
@@ -0,0 +1,137 @@
+//
+//  AdaptiveModalPageViewController+AdaptiveModalPresentationEventsNotifiable.swift
+//  
+//
+//  Created by Dominic Go on 9/7/23.
+//
+
+import UIKit
+
+
+extension AdaptiveModalPageViewController: AdaptiveModalPresentationEventsNotifiable {
+
+  public func notifyOnModalWillSnap(
+    sender: AdaptiveModalManager,
+    prevSnapPointIndex: Int?,
+    nextSnapPointIndex: Int,
+    prevSnapPointConfig: AdaptiveModalSnapPointConfig?,
+    nextSnapPointConfig: AdaptiveModalSnapPointConfig,
+    prevInterpolationPoint: AdaptiveModalInterpolationPoint?,
+    nextInterpolationPoint: AdaptiveModalInterpolationPoint
+  ) {
+  
+    guard let resolvedPages = self.resolvedPages,
+          let nextPage = resolvedPages[safeIndex: nextSnapPointIndex]
+    else { return };
+    
+    let prevPage: AdaptiveModalResolvedPageItemConfig? = {
+      guard let prevSnapPointIndex = prevSnapPointIndex else { return nil };
+      return resolvedPages[safeIndex: prevSnapPointIndex]
+    }();
+  
+    self.pageChangeEventDelegate.invoke {
+      $0.notifyOnModalPageWillChange(
+        sender: self,
+        prevPage: prevPage,
+        nextPage: nextPage
+      );
+    };
+    
+    if prevPage?.viewController !== nextPage.viewController,
+       let delegate = prevPage?.viewController as? AdaptiveModalPageFocusEventsNotifiable {
+    
+      delegate.notifyOnModalPageWillBlur(
+        sender: self,
+        pageToBlur: prevPage,
+        pageToFocus: nextPage
+      );
+    };
+    
+    if let delegate = nextPage.viewController as? AdaptiveModalPageFocusEventsNotifiable {
+      delegate.notifyOnModalPageWillFocus(
+        sender: self,
+        pageToBlur: prevPage,
+        pageToFocus: nextPage
+      );
+    };
+  };
+  
+  public func notifyOnModalDidSnap(
+    sender: AdaptiveModalManager,
+    prevSnapPointIndex: Int?,
+    currentSnapPointIndex: Int,
+    prevSnapPointConfig: AdaptiveModalSnapPointConfig?,
+    currentSnapPointConfig: AdaptiveModalSnapPointConfig,
+    prevInterpolationPoint: AdaptiveModalInterpolationPoint?,
+    currentInterpolationPoint: AdaptiveModalInterpolationPoint
+  ) {
+    
+    guard let resolvedPages = self.resolvedPages,
+          let currentPage = resolvedPages[safeIndex: currentSnapPointIndex]
+    else { return };
+    
+    let prevPage: AdaptiveModalResolvedPageItemConfig? = {
+      guard let prevSnapPointIndex = prevSnapPointIndex else { return nil };
+      return resolvedPages[safeIndex: prevSnapPointIndex]
+    }();
+    
+    self.pageChangeEventDelegate.invoke {
+      $0.notifyOnModalPageDidChange(
+        sender: self,
+        prevPage: prevPage,
+        currentPage: currentPage
+      );
+    };
+    
+    if prevPage?.viewController !== currentPage.viewController,
+       let delegate = prevPage?.viewController as? AdaptiveModalPageFocusEventsNotifiable {
+       
+      delegate.notifyOnModalPageDidBlur(
+        sender: self,
+        blurredPage: prevPage,
+        focusedPage: currentPage
+      );
+    };
+    
+    if let delegate = currentPage.viewController as? AdaptiveModalPageFocusEventsNotifiable {
+      delegate.notifyOnModalPageDidFocus(
+        sender: self,
+        blurredPage: prevPage,
+        focusedPage: currentPage
+      );
+    };
+  };
+  
+  public func notifyOnAdaptiveModalWillShow(sender: AdaptiveModalManager) {
+    self.resolvePages(interpolationPoints: sender.interpolationSteps);
+    self.attachPages();
+  };
+  
+  public func notifyOnAdaptiveModalDidShow(sender: AdaptiveModalManager) {
+    // no-op
+  };
+  
+  public func notifyOnAdaptiveModalWillHide(sender: AdaptiveModalManager) {
+    // no-op
+  };
+  
+  public func notifyOnAdaptiveModalDidHide(sender: AdaptiveModalManager) {
+    // no-op
+  };
+  
+  public func notifyOnModalPresentCancelled(sender: AdaptiveModalManager) {
+    // no-op
+  };
+  
+  public func notifyOnModalDismissCancelled(sender: AdaptiveModalManager) {
+    // no-op
+  };
+  
+  public func notifyOnCurrentModalConfigDidChange(
+    sender: AdaptiveModalManager,
+    currentModalConfig: AdaptiveModalConfig?,
+    prevModalConfig: AdaptiveModalConfig?
+  ) {
+    self.refreshPages();
+  };
+};
