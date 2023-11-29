84f6e8442915a6dd2df3706f57b3961fb086606e
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-29 22:50:12 +0800

    ⚙️ Chore: Update `.gitignore`

--- ExamplePods/Pods/Target Support Files/AdaptiveModal/AdaptiveModal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AdaptiveModal : NSObject
-@end
-@implementation PodsDummy_AdaptiveModal
-@end
--- ExamplePods/Pods/Target Support Files/ComputableLayout/ComputableLayout-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ComputableLayout : NSObject
-@end
-@implementation PodsDummy_ComputableLayout
-@end
