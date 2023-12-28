b8514b4bb1d5d9625a9ce737d6359dc7fac584ba
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-28 11:32:26 +0800

    ⭐️ Impl: Add `AdaptiveModalInterpolationPoint+StringKeyPathMapping`

---
+++ Sources/Extensions+StringKeyPathMapping/AdaptiveModalInterpolationPoint+StringKeyPathMapping.swift
@@ -0,0 +1,56 @@
+//
+//  AdaptiveModalInterpolationPoint+StringKeyPathMapping.swift
+//
+//
+//  Created by Dominic Go on 12/28/23.
+//
+
+import Foundation
+import DGSwiftUtilities
+
+extension AdaptiveModalInterpolationPoint: StringKeyPathMapping {
+
+  public static var partialKeyPathMap: Dictionary<String, PartialKeyPath<Self>> = [
+    "percent": \.percent,
+    "snapPointIndex": \.snapPointIndex,
+    "computedRect": \.computedRect,
+    "modalPadding": \.modalPadding,
+    "allowSnapping": \.allowSnapping,
+    "backgroundTapInteraction": \.backgroundTapInteraction,
+    "secondaryGestureAxisDampingPercent": \.secondaryGestureAxisDampingPercent,
+    "computedModalScrollViewContentInsets": \.computedModalScrollViewContentInsets,
+    "computedModalScrollViewVerticalScrollIndicatorInsets": \.computedModalScrollViewVerticalScrollIndicatorInsets,
+    "computedModalScrollViewHorizontalScrollIndicatorInsets": \.computedModalScrollViewHorizontalScrollIndicatorInsets,
+    "modalTransform": \.modalTransform,
+    "modalBorderWidth": \.modalBorderWidth,
+    "modalBorderColor": \.modalBorderColor,
+    "modalShadowColor": \.modalShadowColor,
+    "modalShadowOffset": \.modalShadowOffset,
+    "modalShadowOpacity": \.modalShadowOpacity,
+    "modalShadowRadius": \.modalShadowRadius,
+    "modalCornerRadius": \.modalCornerRadius,
+    "modalMaskedCorners": \.modalMaskedCorners,
+    "modalOpacity": \.modalOpacity,
+    "modalContentOpacity": \.modalContentOpacity,
+    "modalBackgroundColor": \.modalBackgroundColor,
+    "modalBackgroundOpacity": \.modalBackgroundOpacity,
+    "modalBackgroundVisualEffect": \.modalBackgroundVisualEffect,
+    "modalBackgroundVisualEffectOpacity": \.modalBackgroundVisualEffectOpacity,
+    "modalBackgroundVisualEffectIntensity": \.modalBackgroundVisualEffectIntensity,
+    "modalDragHandleCornerRadius": \.modalDragHandleCornerRadius,
+    "modalDragHandleSize": \.modalDragHandleSize,
+    "modalDragHandleOffset": \.modalDragHandleOffset,
+    "modalDragHandleColor": \.modalDragHandleColor,
+    "modalDragHandleOpacity": \.modalDragHandleOpacity,
+    "backgroundColor": \.backgroundColor,
+    "backgroundOpacity": \.backgroundOpacity,
+    "backgroundVisualEffect": \.backgroundVisualEffect,
+    "backgroundVisualEffectOpacity": \.backgroundVisualEffectOpacity,
+    "backgroundVisualEffectIntensity": \.backgroundVisualEffectIntensity,
+    "modalPaddingAdjusted": \.modalPaddingAdjusted,
+    "isBgVisualEffectSeeThrough": \.isBgVisualEffectSeeThrough,
+    "isBgDimmingViewSeeThrough": \.isBgDimmingViewSeeThrough,
+    "isBgSeeThrough": \.isBgSeeThrough,
+    "derivedBackgroundTapInteraction": \.derivedBackgroundTapInteraction,
+  ];
+};
