3963dfa4b42a24cc8b132d8b33c694453bb894b1
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-21 22:03:57 +0800

    ⭐️ Impl: Add `AdaptiveModalSnapPointType`

---
+++ Sources/AdaptiveModalConfig/AdaptiveModalSnapPointType.swift
@@ -0,0 +1,15 @@
+//
+//  AdaptiveModalSnapPointType.swift
+//  
+//
+//  Created by Dominic Go on 1/21/24.
+//
+
+import Foundation
+
+
+public enum AdaptiveModalSnapPointType: String {
+  case snapPoint;
+  case undershootSnapPoint;
+  case overshootSnapPoint;
+};
